9480e169bbae149b65ca4eac8ded75f63fb7c51e
bachesaismeriem-web <bachesaismeriem@gmail.com>
2026-04-13 12:53:25 +0100

    sql-databse

---
+++ database/chema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE watch_catalog;
+
+USE watch_catalog;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    image_url VARCHAR(500) NOT NULL
+);
