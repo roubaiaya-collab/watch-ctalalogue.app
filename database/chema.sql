99f228b8aa47bfafdfdbd60f06b3a409742b9d60
roubaiaya-collab <roubaiaya80@gmail.com>
2026-04-22 10:06:53 +0200

    Update chema.sql

--- database/chema.sql
+++ database/chema.sql
@@ -7,5 +7,5 @@ CREATE TABLE products (
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    image_url VARCHAR(500) NOT NULL
+    image VARCHAR(500) NOT NULL
 );
