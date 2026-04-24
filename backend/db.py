d3aeab3915e11b2527a2a1adc10c24fac4a44a74
bachesaismeriem-web <bachesaismeriem@gmail.com>
2026-04-24 15:39:23 +0100

    Update database password in db.py

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="2022",
+        password="2720034",
         database="watch_catalog"
     )
